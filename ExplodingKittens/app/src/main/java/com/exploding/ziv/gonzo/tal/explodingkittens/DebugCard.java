b3c7159e60e020f461ef05e4c7341650121d8e7b
taldim <taldim1@gmail.com>
2015-11-14 23:45:43 +0200

    Too late, wer're going home

---
+++ ExplodingKittens/app/src/main/java/com/exploding/ziv/gonzo/tal/explodingkittens/DebugCard.java
@@ -0,0 +1,13 @@
+package com.exploding.ziv.gonzo.tal.explodingkittens;
+
+import android.media.Image;
+import android.widget.ImageView;
+
+/**
+ * Created by tal on 11/14/2015.
+ */
+public class DebugCard implements ICard{
+    public void Draw()
+    {
+    }
+}
---
+++ ExplodingKittens/app/src/main/java/com/exploding/ziv/gonzo/tal/explodingkittens/ICard.java
@@ -0,0 +1,8 @@
+package com.exploding.ziv.gonzo.tal.explodingkittens;
+
+/**
+ * Created by tal on 11/14/2015.
+ */
+public interface ICard {
+    public void Draw();
+}
